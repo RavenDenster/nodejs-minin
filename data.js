865d00088526a177fce46a2427829b2576fa8b59
Oleg Kashechkin <95638297+RavenDenster@users.noreply.github.com>
2023-04-26 15:14:19 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,3 @@
+const text = 'Hello NodeJS from data.js' // модуль в node это отдельный файл и он оборачивается в функцию с параметрами exports, require, module, _dirname, _filename
+
+module.exports = text
